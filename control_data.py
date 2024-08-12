3dae4eb60d0f5e48f46cea0511bf07e4a42fc73f
emvalpe <114688535+emvalpe@users.noreply.github.com>
2024-08-12 15:01:47 -0500

    Update control_data.py

--- control_data.py
+++ control_data.py
@@ -157,7 +157,12 @@ def crude_oil(execution_type):
 
 	for i in data_sources:
 		
-		data = requests.get(starting_url+i).json()
+		try:
+			data = requests.get(starting_url+i).json()
+		except requests.exceptions.JSONDecodeError:
+			print("failed at: " + i)
+			continue
+
 		if execution_type == "update":
 			wdir = open("./crude_oil/"+i.split("/")[1]+".txt", "a+")
 			day = data["dataset"]["data"][0]
